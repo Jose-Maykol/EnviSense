bdc012d5b5cf4ab05b5e3b04fd8d48a489b1b034
José Maykol <jpaniura@unsa.edu.pe>
2023-10-29 16:11:30 -0500

    BUILD: folder structure and app colors

---
+++ lib/constant/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const blue100 = Color(0xFFBDBEFF);
+  static const blue200 = Color(0xFFA8B4FF);
+  static const blue300 = Color(0xFF5478FF);
+  static const blue400 = Color(0xFF0047FB);
+  static const blue500 = Color(0xFF0035BC);
+  static const blue600 = Color(0xFF00247E);
+
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+
+  static const grey100 = Color(0xFFCFCFCF);
+  static const grey200 = Color(0xFFA1A1A1);
+  static const grey300 = Color(0xFF737373);
+  static const grey400 = Color(0xFF565656);
+  static const grey500 = Color(0xFF3A3A3A);
+}

6fbc2b5d0ac4bead0b3b234878aade4c38901bee
José Maykol <jpaniura@unsa.edu.pe>
2023-11-24 22:18:20 -0500

    STYLE: air quality widget to home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -60,6 +60,127 @@ class HomeView extends StatelessWidget {
                 )
               ],
             ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget> [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 150,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      Text(
+                        'Ultima actualización',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: AppColor.grey500,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '14:30',
+                        style: TextStyle(
+                          fontSize: 60,
+                          color: AppColor.grey400,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        '27 de agosto del 2023',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: AppColor.grey200,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    color: AppColor.blue500,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget> [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget> [
+                            const Text(
+                              'Calidad del aire',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: AppColor.grey100,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Container(
+                              width: 20,
+                              height: 20,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: 120,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/icons/air.png'),
+                              fit: BoxFit.contain,
+                              alignment: Alignment.topLeft,
+                            )
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text.rich(
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: '20',
+                                        style: TextStyle(
+                                          fontSize: 30,
+                                          color: AppColor.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: ' PMM',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          color: AppColor.grey100,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),

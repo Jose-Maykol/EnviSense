1d3f4aed9120069733aef80f290688da31b843a0
José Maykol <jpaniura@unsa.edu.pe>
2023-12-25 16:28:13 -0500

    REFACTOR: contacts view and add contact provider

--- lib/view/contacts/contacts_view.dart
+++ lib/view/contacts/contacts_view.dart
@@ -1,12 +1,118 @@
+// ignore_for_file: unused_result
+
 import 'package:airsense/constant/colors.dart';
-import 'package:airsense/models/Contact.dart';
 import 'package:airsense/view/contacts/modals/delete_contact_modal.dart';
 import 'package:airsense/view/contacts/modals/edit_contact_modal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:airsense/widgets/headers/user_header.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:airsense/providers/contact_provider.dart';
 import 'package:flutter/material.dart';
 
-class ContactsView extends StatefulWidget {
+class ContactsView extends ConsumerWidget {
+  const ContactsView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final contactsAsyncValue = ref.watch(contactProvider);
+
+    void handleDeleteContact(BuildContext context, String contactId) {
+      DeleteConfirmationDialog.show(context, contactId, ref);
+    }
+
+    void handleEditContact(BuildContext context, String contactId) {
+      EditContactModal.show(context, contactId, ref);
+    }
+
+    return contactsAsyncValue.when(
+      data: (contacts) {
+        return RefreshIndicator(
+          onRefresh: () async {
+            ref.refresh(contactProvider);
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget> [
+              const UserHeader(),
+              const SizedBox(height: 20),
+              const Text(
+                'Mis contactos',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: AppColor.blue500,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,              
+              ),
+              //const SizedBox(height: 10),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: contacts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  if (contacts.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'No hay contactos',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: AppColor.blue500,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    );
+                  } else {
+                    return ListTile(
+                      isThreeLine: true,
+                      dense: true,
+                      title: Text(contacts[index].name),
+                      subtitle: Text(contacts[index].email),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () async {
+                              print('Edit contact');
+                              handleEditContact(context, contacts[index].id);
+                              ref.refresh(contactProvider);
+                            },
+                            icon: const Icon(
+                              Icons.edit,
+                              color: AppColor.blue300,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              print('Delete contact');
+                              handleDeleteContact(context, contacts[index].id);
+                            },
+                            icon: const Icon(
+                              Icons.delete,
+                              color: AppColor.blue600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              ),
+            ] 
+          )
+            ),
+          ),
+        );
+      },
+      loading: () => Center(child: CircularProgressIndicator()),
+      error: (error, stackTrace) => Center(child: Text('Error: $error')),
+    );
+  }
+}
+
+/* class ContactsView extends StatefulWidget {
   const ContactsView({Key? key}) : super(key: key);
 
   @override
@@ -29,7 +135,9 @@ class _ContactsViewState extends State<ContactsView> {
 
     try {
       print('Getting contacts...');
-      QuerySnapshot querySnapshot = await firestore.collection('contacts').get();
+      User? user = FirebaseAuth.instance.currentUser;
+      QuerySnapshot querySnapshot = await firestore.collection('contacts').where('userId', isEqualTo: user?.uid).get();
+      print(querySnapshot.docs.length);
       querySnapshot.docs.forEach((doc) {
         Contact contact = Contact.fromSnapshot(doc as DocumentSnapshot<Map<String, dynamic>>);
         fetchedContacts.add(contact);
@@ -98,7 +206,14 @@ class _ContactsViewState extends State<ContactsView> {
               itemBuilder: (BuildContext context, int index) {
                 if (contacts.isEmpty) {
                   return const Center(
-                    child: Text('No hay contactos'),
+                    child: Text(
+                      'No hay contactos',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: AppColor.blue500,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   );
                 } else {
                   return ListTile(
@@ -140,4 +255,4 @@ class _ContactsViewState extends State<ContactsView> {
       )
     );
   }
-}
+} */

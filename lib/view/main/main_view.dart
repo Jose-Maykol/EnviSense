8ebfb5dfa107985c66fb920c2a8c47d206c9f9fa
José Maykol <jpaniura@unsa.edu.pe>
2023-12-24 19:50:37 -0500

    FEATURE:  add functionality to add contacts

--- lib/view/main/main_view.dart
+++ lib/view/main/main_view.dart
@@ -1,6 +1,9 @@
 import 'package:airsense/constant/colors.dart';
 import 'package:airsense/view/contacts/contacts_view.dart';
+import 'package:airsense/view/contacts/modals/add_contact_modal.dart';
 import 'package:airsense/view/home/home_view.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatefulWidget {
@@ -13,6 +16,24 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
 
   int _selectedIndex = 0;
+
+  void _handleAddContact(BuildContext context) {
+  AddContactModal.show(context, (String name, String email) async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      // Agregar el nuevo contacto a Firebase
+      await FirebaseFirestore.instance.collection('contacts').add({
+        'name': name,
+        'email': email,
+        'userId': user?.uid,
+      });
+      print('Contacto agregado a Firebase: Nombre: $name, Email: $email');
+    } catch (e) {
+      print('Error al agregar el contacto a Firebase: $e');
+      // Manejar cualquier error que ocurra al agregar el contacto a Firebase
+    }
+  });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +69,9 @@ class _MainViewState extends State<MainView> {
         ],
       ),
       floatingActionButton: _selectedIndex == 1 ? FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _handleAddContact(context);
+        },
         backgroundColor: AppColor.blue500,
         child: const Icon(
           Icons.add,

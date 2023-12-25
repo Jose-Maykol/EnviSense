76262ba6d9ec20efe792eef7eef92719121c6ac9
José Maykol <jpaniura@unsa.edu.pe>
2023-12-25 18:35:11 -0500

    REFACTOR: add styling to delete contact confirmation dialog

--- lib/view/contacts/modals/delete_contact_modal.dart
+++ lib/view/contacts/modals/delete_contact_modal.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously, unused_result
 
+import 'package:airsense/constant/colors.dart';
 import 'package:airsense/providers/contact_provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -11,13 +12,32 @@ class DeleteConfirmationDialog {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Eliminar contacto'),
+          title: const Text(
+            'Eliminar contacto',
+            style: TextStyle(
+              fontSize: 18,
+              color: AppColor.blue500,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
           content: const Text('¿Estás seguro de que deseas eliminar este contacto?'),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.pop(context); // Cierra el diálogo
               },
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: AppColor.blue500,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  )
+                ),
+              ),
               child: const Text('Cancelar'),
             ),
             TextButton(
@@ -26,7 +46,32 @@ class DeleteConfirmationDialog {
                 ref.refresh(contactProvider);
                 Navigator.pop(context); // Cierra el diálogo
               },
-              child: const Text('Eliminar'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(AppColor.blue500),
+                overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.hovered)) {
+                      return AppColor.blue500.withOpacity(0.2);
+                    }
+                    if (states.contains(MaterialState.focused) ||
+                        states.contains(MaterialState.pressed)) {
+                      return AppColor.blue500.withOpacity(0.2);
+                    }
+                    return null; // Defer to the widget's default.
+                  },
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  )
+                ),
+              ),
+              child: const Text(
+                'Eliminar',
+                style: TextStyle(
+                  color: AppColor.white,
+                ),
+              ),
             ),
           ],
         );

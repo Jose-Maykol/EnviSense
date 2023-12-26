3f66fea1c60ded59ba108cfd2396448570aad310
José Maykol <jpaniura@unsa.edu.pe>
2023-12-25 22:47:37 -0500

    FEATURE: add device provider to fetch list of devices

---
+++ lib/providers/device_provider.dart
@@ -0,0 +1,8 @@
+import 'package:airsense/models/device.dart';
+import 'package:airsense/services/firestore_database.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final deviceProvider = FutureProvider.autoDispose<List<Device>>((ref) async {
+  final dynamic listDevices = await FirestoreDatabase().getDevices();
+  return listDevices;
+});
--- lib/services/firestore_database.dart
+++ lib/services/firestore_database.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:airsense/models/contact.dart';
+import 'package:airsense/models/device.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -8,21 +9,35 @@ class FirestoreDatabase {
   Future<List<Contact>> getContacts() async {
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     List<Contact> fetchedContacts = [];
-
     try {
-      print('Getting contacts...');
       User? user = FirebaseAuth.instance.currentUser;
       QuerySnapshot querySnapshot = await firestore.collection('contacts').where('userId', isEqualTo: user?.uid).get();
-      print(querySnapshot.docs.length);
-      querySnapshot.docs.forEach((doc) {
+      for (var doc in querySnapshot.docs) {
         Contact contact = Contact.fromSnapshot(doc as DocumentSnapshot<Map<String, dynamic>>);
         fetchedContacts.add(contact);
-        print(contact.id );
-      });
+      }
       return fetchedContacts;
     } catch (e) {
       print('Error getting contacts: $e');
       return fetchedContacts;
+    }
+  }
+
+  Future<List<Device>> getDevices() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    List<Device> fetchedDevices = [];
+    try {
+      // User? user = FirebaseAuth.instance.currentUser;
+      // QuerySnapshot querySnapshot = await firestore.collection('devices').where('userId', isEqualTo: user?.uid).get();
+      QuerySnapshot querySnapshot = await firestore.collection('devices').get();
+      for (var doc in querySnapshot.docs) {
+        Device device = Device.fromSnapshot(doc as DocumentSnapshot<Map<String, dynamic>>);
+        fetchedDevices.add(device);
+      }
+      return fetchedDevices;
+    } catch (e) {
+      print('Error getting devices: $e');
+      return fetchedDevices;
     }
   }
 }

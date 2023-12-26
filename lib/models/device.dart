9949f55d82262b66fa16782fe94ec60fca24692e
José Maykol <jpaniura@unsa.edu.pe>
2023-12-26 13:03:02 -0500

    REFACTOR: add name and unit properties to device

--- lib/models/device.dart
+++ lib/models/device.dart
@@ -5,13 +5,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Device {
   final String id;
   final String mac;
+  final String name;
   final String type;
+  final String unit;
   final String userId;
 
   Device({
     required this.id,
     required this.mac,
+    required this.name,
     required this.type,
+    required this.unit,
     required this.userId,
   });
 
@@ -19,7 +23,9 @@ class Device {
     return {
       'id': id,
       'mac': mac,
+      'name': name,
       'type': type,
+      'unit': unit,
       'userId': userId,
     };
   }
@@ -27,8 +33,10 @@ class Device {
   factory Device.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot) {
     return Device(
       id: snapshot.id,
-      mac: snapshot.data()?['mac'] ?? '',
-      type: snapshot.data()?['type'] ?? '',
+      mac: snapshot.data()?['deviceMAC'] ?? '',
+      name: snapshot.data()?['deviceName'] ?? '',
+      type: snapshot.data()?['deviceType'].substring(0, 1).toUpperCase() + snapshot.data()?['deviceType'].substring(1) ?? '',
+      unit: snapshot.data()?['deviceUnit'] ?? '',
       userId: snapshot.data()?['userId'] ?? '',
     );
   }

f94e8ea9dfe997fff687fc25e99790f7811bb48f
José Maykol <jpaniura@unsa.edu.pe>
2024-01-08 23:08:42 -0500

    REFACTOR: display real-time device update information

--- lib/view/device/widgets/device_update_information.dart
+++ lib/view/device/widgets/device_update_information.dart
@@ -1,48 +1,72 @@
 import 'package:airsense/constant/colors.dart';
+import 'package:airsense/utils/date.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 
 class DeviceUpdateInformation extends StatelessWidget {
-  const DeviceUpdateInformation({
+
+  final DatabaseReference _deviceRef;
+  final String deviceId;
+
+  DeviceUpdateInformation({
     super.key,
-  });
+    required this.deviceId,
+  }):
+    _deviceRef = FirebaseDatabase.instance.ref().child('devices/$deviceId');
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.45,
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          Text(
-            'Ultima actualización',
-            style: TextStyle(
-              fontSize: 16,
-              color: AppColor.grey500,
-              fontWeight: FontWeight.bold,
+    return StreamBuilder(
+      stream: _deviceRef.onValue,
+      builder: (context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          final deviceData = snapshot.data!.snapshot.value;
+          final timestamp = deviceData['timestamp'];
+          String time = getFormattedTime(timestamp);
+          String date = getFormattedDate(timestamp);
+
+          return SizedBox(
+            width: MediaQuery.of(context).size.width * 0.45,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget> [
+                const Text(
+                  'Ultima actualización',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: AppColor.grey500,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  time,
+                  style: const TextStyle(
+                    fontSize: 60,
+                    color: AppColor.grey400,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  date,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: AppColor.grey200,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            '14:30',
-            style: TextStyle(
-              fontSize: 60,
-              color: AppColor.grey400,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 5),
-          Text(
-            '27 de agosto del 2023',
-            style: TextStyle(
-              fontSize: 16,
-              color: AppColor.grey200,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
